b6bb7af70eb3e398c6bc59673fd5d97d345464f4
miafernandez14 <102004209+miafernandez14@users.noreply.github.com>
2023-06-27 17:50:21 -0500

    Delete Lab14Web_Cambio-master directory

--- Lab14Web_Cambio-master/routes/pdf.js
+++
@@ -1,9 +0,0 @@
-//Rutas producto
-const express = require('express');
-const router = express.Router();
-const pdfController = require('../Controller/pdfController');
-
-
-router.get('/', pdfController.generarPDf);
-
-module.exports = router;
