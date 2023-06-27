69ee8e444ff18886b7d843c2aa801d03614297c5
miafernandez14 <mia.fernandez@tecsup.edu.pe>
2023-06-27 16:02:05 -0500

    1 commt

--- Lab14Web_Cambio-master/index.js
+++ Lab14Web_Cambio-master/index.js
@@ -24,25 +24,3 @@ app.use('/api/pdf', verifyToken, require('./routes/pdf'));
 app.listen(config.port, () => {
     console.log('El servidor por el puerto 4000')
 })
-
-async function verifyToken(req, res, next) {
-	try {
-		if (!req.headers.authorization) {
-			return res.status(401).send('Unauhtorized Request');
-		}
-		let token = req.headers.authorization.split(' ')[1];
-		if (token === 'null') {
-			return res.status(401).send('Unauhtorized Request');
-		}
-
-		const payload = await jwt.verify(token, 'secretkey');
-		if (!payload) {
-			return res.status(401).send('Unauhtorized Request');
-		}
-		req.userId = payload._id;
-		next();
-	} catch(e) {
-		//console.log(e)
-		return res.status(401).send('Unauhtorized Request');
-	}
-}
