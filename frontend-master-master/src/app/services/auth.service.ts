d30f8656185cfea32c94ae3ca010317836c4ba92
angelaceles <85888474+angelaceles@users.noreply.github.com>
2023-06-28 20:46:02 -0500

    Update auth.service.ts

--- frontend-master-master/src/app/services/auth.service.ts
+++ frontend-master-master/src/app/services/auth.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
 
 
 @Injectable({
@@ -7,7 +8,19 @@ import { Injectable } from '@angular/core';
 export class AuthService {
 
   private URL = 'http://localhost:4000/api'
-  constructor() { }
+  constructor(private router: Router) { }
 
   signUp( email: string, password: string ){}
+
+  loggedIn(): Boolean {
+    return !!localStorage.getItem('token');
+  }
+  logout(){
+    localStorage.removeItem('token');
+    this.router.navigate(['/signin'])
+  }
+
+  getToken(){
+    return localStorage.getItem('token');
+  }
 }
